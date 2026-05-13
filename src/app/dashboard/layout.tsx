c1316758fa5fb4586822bee3f5ff72a8027a4882
Marta Dominik <marta@slowey.ie>
2026-05-13 19:27:01 +0100

    feats: initial project scaffold

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout({
 }) {
     return (
         <div className="flex flex-col md:flex-row min-h-screen bg-gray-50">
-            <Sidebar />
+            {/* <Sidebar /> */}
 
             <main className="flex-1 overflow-y-auto">
                 {/* Adds padding to prevent content hiding behind mobile header */}
